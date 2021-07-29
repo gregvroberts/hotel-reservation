2b773f90c180c90e2e5ce7c3a7005a3937ebf324
Gregory Roberts <roberts.gregory@heb.com>
2021-07-29 17:18:46 -0500

    Revert "contentful"
    This reverts commit bd4c1c55

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
 import './App.css';
+
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+
 import {Route, Switch} from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 
-
-import {Router} from 'react-router-dom';
-import ReactGA from 'react-ga';
-import {createBrowserHistory} from 'history';
-
 function App() {
-    const history = createBrowserHistory();
-
-    history.listen(location => {
-        ReactGA.initialize('G-QK4RZWGQV9');
-        ReactGA.set({ page: location.pathname }); // Update the user's current page
-        ReactGA.pageview(location.pathname); // Record a pageview for the given page
-    });
-
-
-    return (
-
-        <Router history={history}>
-            <>
-                <Navbar/>
-                <Switch>
-                    <Route exact path={"/"} component={Home}/>
-                    <Route exact path={"/rooms"} component={Rooms}/>
-                    <Route exact path={"/rooms/:slug"} component={SingleRoom}/>
-                    <Route component={Error}/>
-                </Switch>
-            </>
-        </Router>
-    );
+  return (
+      <>
+          <Navbar />
+          <Switch>
+              <Route exact path={"/"} component={Home}/>
+              <Route exact path={"/rooms"} component={Rooms}/>
+              <Route exact path={"/rooms/:slug"} component={SingleRoom}/>
+              <Route component={Error} />
+          </Switch>
+      </>
+  );
 }
 
 export default App;
