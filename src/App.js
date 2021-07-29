bd4c1c5566effeacbd16741e46ddc7e1e0293924
Gregory Roberts <roberts.gregory@heb.com>
2021-07-29 17:02:25 -0500

    contentful

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import './App.css';
-
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
-
 import {Route, Switch} from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 
+
+import {BrowserRouter as Router} from 'react-router-dom';
+import ReactGA from 'react-ga';
+import {createBrowserHistory} from 'history';
+
 function App() {
-  return (
-      <>
-          <Navbar />
-          <Switch>
-              <Route exact path={"/"} component={Home}/>
-              <Route exact path={"/rooms"} component={Rooms}/>
-              <Route exact path={"/rooms/:slug"} component={SingleRoom}/>
-              <Route component={Error} />
-          </Switch>
-      </>
-  );
+    const history = createBrowserHistory();
+
+    history.listen(location => {
+        ReactGA.initialize('your tracking Id');
+        ReactGA.set({ page: location.pathname }); // Update the user's current page
+        ReactGA.pageview(location.pathname); // Record a pageview for the given page
+    });
+
+
+    return (
+
+        <Router history={history}>
+            <>
+                <Navbar/>
+                <Switch>
+                    <Route exact path={"/"} component={Home}/>
+                    <Route exact path={"/rooms"} component={Rooms}/>
+                    <Route exact path={"/rooms/:slug"} component={SingleRoom}/>
+                    <Route component={Error}/>
+                </Switch>
+            </>
+        </Router>
+    );
 }
 
 export default App;
